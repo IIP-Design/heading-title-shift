a9093b953b2f66066557c562d9bb1cc4f3254e72
mrewers <marekrewers@gmail.com>
2018-04-03 16:52:37 -0400

    Clean up inputs section

--- iip-double-author.php
+++ iip-double-author.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name: Second Author Plugin for IIP Properties
+ * Plugin Name: Double Author Plugin for IIP Properties
  * Description: Adds extra metabox to admin panel which can be used to add a second author to a post byline
  * Version: 1.0.0
  * Author: Marek Rewers
  * Text Domain: iip-double-author
- * License: GPLv2 or later
+ * License: GPLv2 or laters
  */
 
-// Adds a meta box to the post editing screen
+// Adds a second author metabox to the post editing screen
 function double_author_custom_meta() {
   add_meta_box( 'double_author_meta', __( 'Second Author', 'iip-double-author' ), 'double_author_meta_callback', 'post' );
 }
@@ -16,28 +16,49 @@ function double_author_custom_meta() {
 add_action( 'add_meta_boxes', 'double_author_custom_meta' );
 
 
-// Callback for the content of the meta box
+// Callback for the content of the metabox
 function double_author_meta_callback( $post ) {
  	wp_nonce_field( basename( __FILE__ ), 'double_author_nonce' );
- 	$double_author_stored_meta = get_post_meta( $post->ID );
+ 	$double_author_value = get_post_meta( $post->ID, "_iip_add_second_author", true );
  	?>
 
- 	<p>
- 		<div class="double-author-row-content">
-      <span class="double-author-row-title"><?php _e( 'Add second author?', 'iip-double-author' )?></span>
- 			<label for="meta-radio-one">
- 				<input type="radio" name="meta-radio" id="meta-radio-one" value="radio-one" <?php if ( isset ( $double_author_stored_meta['meta-radio'] ) ) checked( $double_author_stored_meta['meta-radio'][0], 'radio-one' ); ?>>
- 				<?php _e( 'Yes', 'iip-double-author' )?>
-      </label>
-      <label for="meta-radio-two">
-				<input type="radio" name="meta-radio" id="meta-radio-two" value="radio-two" <?php if ( isset ( $double_author_stored_meta['meta-radio'] ) ) checked( $double_author_stored_meta['meta-radio'][0], 'radio-two' ); ?>>
-				<?php _e( 'No', 'iip-doubleauthor' )?>
-			</label>
- 		</div>
- 	</p>
+	<div class="double-author-row-content">
+    <span class="double-author-row-title"><?php _e( 'Add second author?', 'iip-double-author' )?></span><br/>
 
- 	<?php
- }
+		<div class="double-author-options" style="margin: 10px 0 15px 5px;">
+      <input
+        type="radio"
+        id="double-author-yes"
+        name="_iip_add_second_author"
+        value="yes"
+        style="margin-top:-1px; vertical-align:middle;"
+        <?php checked( $double_author_value, 'yes' ); ?>
+      />
+      <label for="double-author-radio-yes"><?php _e( 'Yes', 'iip-double-author' )?></label>
+
+			<input
+        type="radio"
+        id="double-author-no"
+        name="_iip_add_second_author"
+        value="no"
+        style="margin-top:-1px; margin-left: 10px; vertical-align:middle;"
+        <?php checked( $double_author_value, '' ); ?>
+        <?php checked( $double_author_value, 'no' ); ?>
+      />
+			<label for="double-author-radio-no"><?php _e( 'No', 'iip-double-author' )?></label>
+
+    </div> <!-- End double-author-options -->
+  </div> <!-- End double-author-row-content-->
+
+  <?php
+  $double_author_id = get_post_meta( $post->ID, "_iip_post_second_author", true );
+	wp_dropdown_users( array(
+		'name' => '_iip_post_second_author',
+		'selected' => $double_author_id,
+		'include_selected' => true,
+		'show' => 'display_name',
+	) );
+  }
 
 // Saves the custom meta input
 function double_author_meta_save( $post_id ) {
@@ -53,8 +74,12 @@ function double_author_meta_save( $post_id ) {
  	}
 
  	// Checks for input and saves if needed
- 	if( isset( $_POST[ 'meta-radio' ] ) ) {
- 		update_post_meta( $post_id, 'meta-radio', $_POST[ 'meta-radio' ] );
+ 	if( isset( $_POST[ '_iip_add_second_author' ] ) ) {
+ 		update_post_meta( $post_id, '_iip_add_second_author', $_POST[ '_iip_add_second_author' ] );
+ 	}
+
+  if( isset( $_POST[ '_iip_post_second_author' ] ) ) {
+ 		update_post_meta( $post_id, '_iip_post_second_author', $_POST[ '_iip_post_second_author' ] );
  	}
  }
 
